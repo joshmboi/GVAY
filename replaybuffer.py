a8206be8592a0d56fee8d9348e5a1728d494e442
Joshua Boisvert <joshmboisvert@gmail.com>
2025-06-04 03:05:14 -0700

    update more frequently while rollout

--- replaybuffer.py
+++ replaybuffer.py
@@ -13,46 +13,95 @@ class ReplayBuffer:
         self.window = window
         self.fpa = fpa
 
+        # indices of starts of episodes
+        self.inds = []
+
+        # doublely-ended queues for automatic cap size
+        self.obs = deque()
+        self.ac_idxs = deque()
+        self.ac_poses = deque()
+        self.rews = deque()
+        self.dones = deque()
+
         self.rolls = deque()
 
-    def add_rolls(self, rolls):
-        # add all rollouts
-        for roll in rolls:
-            self.rolls.append(roll)
-            self.cur_steps += roll.num_steps
+    def add(self, obs, ac_idx, ac_pos, rew, done):
+        # add index if beginning of episode (observation length of window)
+        if len(obs) == self.window:
+            self.inds.append(len(self.dones))
 
-        # take away rollouts until fractional episode over cap
-        while self.rolls[0].num_steps <= self.cur_steps - self.cap:
-            roll = self.rolls.popleft()
-            self.cur_steps -= roll.num_steps
+        # append each observation
+        for ob in obs:
+            self.obs.append(ob)
+
+        # append the actions, rewards and dones
+        self.ac_idxs.append(ac_idx)
+        self.ac_poses.append(ac_pos)
+        self.rews.append(rew)
+        self.dones.append(done)
+
+        if len(self.ac_idxs) > self.cap:
+            # reduce index of all indices
+            for i in range(len(self.inds)):
+                self.inds[i] -= 1
+
+            # check if finished episode
+            if self.dones[0]:
+                self.inds.popleft()
+
+                for _ in range(self.window):
+                    self.obs.popleft()
+            else:
+                for _ in range(self.window - self.fpa):
+                    self.obs.popleft()
+
+            # pop to keep size
+            self.ac_idxs.popleft()
+            self.ac_poses.popleft()
+            self.rews.popleft()
+            self.dones.popleft()
+
+    def reassign_rew(self, rew):
+        self.rews[-1] = rew
+
+    def reassign_done(self):
+        self.dones[-1] = True
+
+    def num_epis(self, idx):
+        i = 0
+        while i < len(self.inds) and idx >= self.inds[i]:
+            i += 1
+        return i - 1
 
     def sample(self, batch_size):
+        print(len(self.obs), len(self.ac_idxs), self.inds)
+
         batch = []
         while len(batch) < batch_size:
-            roll = random.choice(self.rolls)
+            # random index for sampling
+            idx = random.randint(0, len(self.ac_idxs) - 1)
 
-            # choose action step
-            step = random.choice(range(roll.num_steps))
+            extra_idx = self.num_epis(idx) * (self.window - self.fpa)
 
-            ob_end = (step + 2) * self.fpa
-            nob_end = (step + 3) * self.fpa
+            ob_start = extra_idx + idx * self.fpa
+            nob_start = extra_idx + (idx + 1) * self.fpa
 
-            # skip transitions that go beyond recorded
-            if nob_end > len(roll.observations):
-                continue
+            ob = []
+            for i in range(ob_start, ob_start + self.window):
+                print(i, self.num_epis(idx), idx)
+                ob.append(self.obs[i])
+            ac_idx = self.ac_idxs[idx]
+            ac_pos = self.ac_poses[idx]
+            rew = self.rews[idx]
+            done = self.dones[idx]
 
-            # get ob, ac, rew, and done
-            ob = roll.observations[ob_end - self.window:ob_end]
-            ac_idx = roll.action_indexes[step]
-            ac_pos = roll.action_positions[step]
-            rew = roll.rewards[step]
-            done = roll.dones[step]
-
-            # tensor of zeros for nob if terminal action
-            if done:
+            if nob_start + self.window > len(self.obs):
                 nob = [np.zeros_like(ob[0]) for i in range(self.window)]
             else:
-                nob = roll.observations[nob_end - self.window:nob_end]
+                nob = []
+                for i in range(nob_start, nob_start + self.window):
+                    print(i, done)
+                    nob.append(self.obs[i])
 
             batch.append((ob, ac_idx, ac_pos, rew, nob, done))
 
@@ -66,3 +115,58 @@ class ReplayBuffer:
             np.array(nobs),
             np.array(dones)
         )
+
+    def __len__(self):
+        return max(len(self.ac_idxs) - 1, 0)
+
+    # def add_rolls(self, rolls):
+    #     # add all rollouts
+    #     for roll in rolls:
+    #         self.rolls.append(roll)
+    #         self.cur_steps += roll.num_steps
+
+    #     # take away rollouts until fractional episode over cap
+    #     while self.rolls[0].num_steps <= self.cur_steps - self.cap:
+    #         roll = self.rolls.popleft()
+    #         self.cur_steps -= roll.num_steps
+
+    # def sample(self, batch_size):
+    #     batch = []
+    #     while len(batch) < batch_size:
+    #         roll = random.choice(self.rolls)
+
+    #         # choose action step
+    #         step = random.choice(range(roll.num_steps))
+
+    #         ob_end = (step + 2) * self.fpa
+    #         nob_end = (step + 3) * self.fpa
+
+    #         # skip transitions that go beyond recorded
+    #         if nob_end > len(roll.observations):
+    #             continue
+
+    #         # get ob, ac, rew, and done
+    #         ob = roll.observations[ob_end - self.window:ob_end]
+    #         ac_idx = roll.action_indexes[step]
+    #         ac_pos = roll.action_positions[step]
+    #         rew = roll.rewards[step]
+    #         done = roll.dones[step]
+
+    #         # tensor of zeros for nob if terminal action
+    #         if done:
+    #             nob = [np.zeros_like(ob[0]) for i in range(self.window)]
+    #         else:
+    #             nob = roll.observations[nob_end - self.window:nob_end]
+
+    #         batch.append((ob, ac_idx, ac_pos, rew, nob, done))
+
+    #     # convert to friendly format
+    #     obs, ac_idxs, ac_poses, rews, nobs, dones = zip(*batch)
+    #     return (
+    #         np.array(obs),
+    #         np.array(ac_idxs),
+    #         np.array(ac_poses),
+    #         np.array(rews).astype(np.float32),
+    #         np.array(nobs),
+    #         np.array(dones)
+    #     )
--- trainer.py
+++ trainer.py
@@ -1,8 +1,9 @@
+from collections import deque
+
 import consts as consts
 
 from game import Game
 from policy import Policy
-from rollout import Roll
 from replaybuffer import ReplayBuffer
 from logger import Logger
 
@@ -10,6 +11,8 @@ from logger import Logger
 class Trainer:
     def __init__(self):
         self.tot_steps = 0
+        self.max_steps = 1000000
+        self.critic_updates = 0
         self.rebuff = ReplayBuffer(consts.REBUFF_SIZE)
 
         self.logger = Logger(name="sac")
@@ -25,22 +28,17 @@ class Trainer:
         self.ac_mask = [0, 0, 1, 0, 0]
         self.game = Game(True, ac_mask=self.ac_mask)
 
-    def sim_roll(self, max_ep_len=1800, eps=0, render=False):
+    def sim_roll(self, max_ep_len=1080, render=False):
         # initialize steps and frames
-        steps = 0
+        steps = 1
         frames = []
 
         # get initial game state
         ob = self.game.reset()
 
-        # initialize reward accumulation
-        tot_p_rew, tot_e_rew = 0.0, 0.0
-
         # initialize capturing lists
-        p_obs, p_ac_idxs, p_ac_poses = [], [], []
-        p_rews, p_dones = [], []
-        e_obs, e_ac_idxs, e_ac_poses = [], [], []
-        e_rews, e_dones = [], []
+        p_obs = []
+        e_obs = []
 
         while True:
             # add observations
@@ -67,356 +65,203 @@ class Trainer:
                 playing=False, p_ac=p_ac, e_ac=e_ac
             )
 
-            # only when action taking turn
-            if p_ac is not None and e_ac is not None:
-                # add actions
-                p_ac_idxs.append(p_ac[0])
-                e_ac_idxs.append(e_ac[0])
-                p_ac_poses.append(p_ac[1])
-                e_ac_poses.append(e_ac[1])
-
-                # rewards assigning to actions
-                if len(p_dones) != 0:
-                    # add rewards and reset
-                    p_rews.append(tot_p_rew)
-                    e_rews.append(tot_e_rew)
-                tot_p_rew, tot_e_rew = 0.0, 0.0
-
-                tot_p_rew += rew["player"]
-                tot_e_rew += rew["enemy"]
-
-                # add done flag
-                p_dones.append(done)
-                e_dones.append(done)
-            else:
-                # accumulate rewards
-                tot_p_rew += rew["player"]
-                tot_e_rew += rew["enemy"]
-
             # end episode if someone dies or hit max episode length
             if steps >= max_ep_len or done:
-                if steps % consts.FPA != 0:
-                    # add assign termination credit to last action
-                    p_dones[-1] = True
-                    e_dones[-1] = True
                 break
 
             # increment steps
             steps += 1
 
-        # add last observation
         p_obs.append(ob["player"])
         e_obs.append(ob["enemy"])
-
-        # add rewards for last action
-        p_rews.append(tot_p_rew)
-        e_rews.append(tot_p_rew)
-
-        # return in friendly format
-        p_roll = Roll(p_obs, p_ac_idxs, p_ac_poses, p_rews, p_dones)
-        e_roll = Roll(e_obs, e_ac_idxs, e_ac_poses, e_rews, e_dones)
 
         if render:
             self.logger.log_video(frames, f"{self.tot_steps}.mp4")
 
-        return p_roll, e_roll, steps
+    def train(self, max_ep_len=1080):
+        done = True
+        first = True
+        accum_p_rew_list = deque(maxlen=10)
 
-    def sim_n_rolls(self, num_rolls, max_ep_len, render=False):
-        # track mean rewards over eval rollouts
-        rews = []
-        for i in range(num_rolls):
-            if i > 0:
-                render = False
-            p_roll, _, _ = self.sim_roll(max_ep_len, 0, render)
+        while self.tot_steps < self.max_steps:
+            if done:
+                # initialize steps and frames
+                steps = 1
 
-            rews.append(sum(p_roll.rewards))
+                # get initial game state
+                ob = self.game.reset()
 
-        # log return stats
-        self.logger.log_scalar(
-            "Eval/Mean Return:",
-            sum(rews) / len(rews),
-            self.eval_step
-        )
-        self.logger.log_scalar(
-            "Eval/Max Return:",
-            max(rews),
-            self.eval_step
-        )
-        self.logger.log_scalar(
-            "Eval/Min Return:",
-            min(rews),
-            self.eval_step
-        )
+                # initialize reward accumulation
+                tot_p_rew, tot_e_rew = 0.0, 0.0
 
-        # print out return stats
-        print(f"Eval/Mean Return: {sum(rews) / len(rews)}")
-        print(f"Eval/Max Return: {max(rews)}")
-        print(f"Eval/Min Return: {min(rews)}")
-        print()
+                # reset total rewards
+                accum_p_rew = 0.0
+                if len(accum_p_rew_list) > 0:
+                    print(f"Eval/Mean Return: {sum(accum_p_rew_list) / len(accum_p_rew_list)}")
 
-        self.eval_step += 1
+                # reset done and first
+                done = False
+                first = True
 
-    def sim_rolls(self, batch_min_steps, max_ep_len, render=False):
-        # initialize
-        iter_steps = 0
-        rolls = []
-        rews = []
+                # initialize capturing lists
+                p_obs = deque(maxlen=consts.WINDOW)
+                e_obs = deque(maxlen=consts.WINDOW)
 
-        while iter_steps < batch_min_steps:
-            # simulate player and enemy rollouts for a game
-            p_roll, e_roll, steps = self.sim_roll(max_ep_len, render)
-            iter_steps += steps * 2
+            # add observations
+            p_obs.append(ob["player"])
+            e_obs.append(ob["enemy"])
 
-            # add to rollouts
-            rolls.append(p_roll)
-            rolls.append(e_roll)
+            p_ac, e_ac = None, None
 
-            # add rewards
-            rews.append(sum(p_roll.rewards))
+            # get action and take it if acting frame
+            if steps % consts.FPA == 0 and steps >= consts.WINDOW:
+                # poll policy
+                p_ac = self.p_pol.get_action(p_obs[-1], self.ac_mask)
+                e_ac = self.e_pol.get_action(e_obs[-1], self.ac_mask)
 
-        # log return stats
-        self.logger.log_scalar(
-            "Sim/Mean Return:",
-            sum(rews) / len(rews),
-            self.iters
-        )
-        self.logger.log_scalar(
-            "Sim/Max Return:",
-            max(rews),
-            self.iters
-        )
-        self.logger.log_scalar(
-            "Sim/Min Return:",
-            min(rews),
-            self.iters
-        )
-
-        # print out return stats
-        print(f"Sim/Mean Return: {sum(rews) / len(rews)}")
-        print(f"Sim/Max Return: {max(rews)}")
-        print(f"Sim/Min Return: {min(rews)}")
-        print()
-
-        return rolls, iter_steps
-
-    def train(self, num_train_steps):
-        # initialize metrics tracking
-        c_losses, q_vals, a_losses, pos_means_x, pos_means_y = [], [], [], [], []
-        alphs, entropies, alph_losses, pos_stds_x, pos_stds_y = [], [], [], [], []
-        c_loss_min, c_loss_max, a_loss_min, a_loss_max = None, None, None, None
-        q_max, q_min, entropy_min, entropy_max = None, None, None, None
-
-        for i in range(num_train_steps):
-            batch = self.rebuff.sample(consts.BATCH_SIZE)
-
-            c_metrics = self.p_pol.update_critic(batch, ac_mask=self.ac_mask)
-
-            c_losses.append(c_metrics["critic_loss"])
-            q_vals.append(c_metrics["q_values"])
-
-            if not c_loss_min:
-                c_loss_min = c_metrics["critic_loss"]
-                c_loss_max = c_metrics["critic_loss"]
-                q_min = c_metrics["q_val_min"]
-                q_max = c_metrics["q_val_max"]
             else:
-                c_loss_min = min(c_loss_min, c_metrics["critic_loss"])
-                c_loss_max = max(c_loss_max, c_metrics["critic_loss"])
-                q_min = min(q_min, c_metrics["q_val_min"])
-                q_max = max(q_max, c_metrics["q_val_max"])
+                self.p_pol.update_actor_hidden(p_obs[-1])
+                self.e_pol.update_actor_hidden(e_obs[-1])
 
-            if self.iters >= consts.CRITIC_ONLY and i % 4 == 0:
-                a_metrics = self.p_pol.update_actor(
-                    batch, ac_mask=self.ac_mask
+            # move forward step
+            ob, rew, done, info = self.game.step(
+                playing=False, p_ac=p_ac, e_ac=e_ac
+            )
+
+            # only when action taking turn
+            if p_ac is not None and e_ac is not None:
+                # add to replay buffer
+                if first:
+                    self.rebuff.add(
+                        list(p_obs), p_ac[0], p_ac[1], tot_p_rew, done
+                    )
+                    first = False
+                else:
+                    self.rebuff.add(
+                        list(p_obs)[-consts.FPA:], p_ac[0],
+                        p_ac[1], tot_p_rew, done
+                    )
+
+                # TODO: enemy transitions too??
+
+                # rewards assigning to actions
+                if steps != consts.WINDOW:
+                    # last rewards and reset
+                    self.rebuff.reassign_rew(tot_p_rew)
+
+                tot_p_rew, tot_e_rew = 0.0, 0.0
+
+                tot_p_rew += rew["player"]
+                tot_e_rew += rew["enemy"]
+                accum_p_rew += rew["player"]
+            else:
+                # accumulate rewards
+                tot_p_rew += rew["player"]
+                tot_e_rew += rew["enemy"]
+                accum_p_rew += rew["player"]
+
+            # end episode if someone dies or hit max episode length
+            if steps >= max_ep_len or done:
+                # add assign termination credit for Bellman updates
+                self.rebuff.reassign_done()
+
+                # add rewards for last action
+                self.rebuff.reassign_rew(tot_p_rew)
+
+                # add accumulated rewards
+                accum_p_rew_list.append(accum_p_rew)
+
+                # set done
+                done = True
+
+            if (
+                steps % consts.FPA == 0 and
+                len(self.rebuff) >= consts.BATCH_SIZE * 10
+            ):
+                batch = self.rebuff.sample(consts.BATCH_SIZE)
+                c_metrics = self.p_pol.update_critic(batch, self.ac_mask)
+
+                self.logger.log_scalar(
+                    "Critic Loss",
+                    c_metrics["critic_loss"],
+                    self.tot_steps
+                )
+                self.logger.log_scalar(
+                    "Mean Q Value",
+                    c_metrics["q_values"],
+                    self.tot_steps
                 )
 
-                a_losses.append(a_metrics["actor_loss"])
-                alphs.append(a_metrics["alpha"])
-                entropies.append(a_metrics["entropy"])
-                alph_losses.append(a_metrics["alpha_loss"])
-                pos_means_x.append(a_metrics["means_x"])
-                pos_stds_x.append(a_metrics["stds_x"])
-                pos_means_y.append(a_metrics["means_y"])
-                pos_stds_y.append(a_metrics["stds_y"])
+                self.critic_updates += 1
 
-                if not a_loss_min:
-                    a_loss_min = a_metrics["actor_loss"]
-                    a_loss_max = a_metrics["actor_loss"]
-                    entropy_min = a_metrics["entropy"]
-                    entropy_max = a_metrics["entropy"]
-                else:
-                    a_loss_min = min(a_loss_min, a_metrics["actor_loss"])
-                    a_loss_max = max(a_loss_max, a_metrics["actor_loss"])
-                    entropy_min = min(entropy_min, a_metrics["entropy"])
+                if (
+                    self.critic_updates % 2 == 0 and
+                    self.tot_steps > consts.CRITIC_ONLY
+                ):
+                    a_metrics = self.p_pol.update_actor(batch, self.ac_mask)
 
-        critic_steps = len(c_losses)
-        actor_steps = len(a_losses)
+                    self.logger.log_scalar(
+                        "Actor Loss",
+                        a_metrics["actor_loss"],
+                        self.tot_steps
+                    )
 
-        self.logger.log_scalar(
-            "Critic/Mean Loss",
-            sum(c_losses) / critic_steps,
-            self.train_step
-        )
-        self.logger.log_scalar(
-            "Critic/Highest Loss",
-            c_loss_max,
-            self.train_step
-        )
-        self.logger.log_scalar(
-            "Critic/Lowest Loss",
-            c_loss_min,
-            self.train_step
-        )
+                    self.logger.log_scalar(
+                        "Entropy",
+                        a_metrics["entropy"],
+                        self.tot_steps
+                    )
 
-        self.logger.log_scalar(
-            "Q/Mean Value",
-            sum(q_vals) / critic_steps,
-            self.train_step
-        )
-        self.logger.log_scalar(
-            "Q/Highest Value",
-            q_max,
-            self.train_step
-        )
-        self.logger.log_scalar(
-            "Q/Lowest Value",
-            q_min,
-            self.train_step
-        )
+                    self.logger.log_scalar(
+                        "Alpha Value",
+                        a_metrics["alpha"],
+                        self.tot_steps
+                    )
+                    self.logger.log_scalar(
+                        "Alpha Loss",
+                        a_metrics["alpha_loss"],
+                        self.tot_steps
+                    )
 
-        if self.iters >= consts.CRITIC_ONLY:
-            self.logger.log_scalar(
-                "Actor/Mean Loss",
-                sum(a_losses) / actor_steps,
-                self.train_step
-            )
-            self.logger.log_scalar(
-                "Actor/Highest Loss",
-                a_loss_max,
-                self.train_step
-            )
-            self.logger.log_scalar(
-                "Actor/Lowest Loss",
-                a_loss_min,
-                self.train_step
-            )
+                    self.logger.log_scalar(
+                        "Position/X Mean",
+                        a_metrics["means_x"],
+                        self.tot_steps
+                    )
+                    self.logger.log_scalar(
+                        "Position/X Std",
+                        a_metrics["stds_x"],
+                        self.tot_steps
+                    )
+                    self.logger.log_scalar(
+                        "Position/Y Mean",
+                        a_metrics["means_y"],
+                        self.tot_steps
+                    )
+                    self.logger.log_scalar(
+                        "Position/Y Std",
+                        a_metrics["stds_y"],
+                        self.tot_steps
+                    )
 
-            self.logger.log_scalar(
-                "Entropy/Mean",
-                sum(entropies) / actor_steps,
-                self.train_step
-            )
-            self.logger.log_scalar(
-                "Entropy/Highest",
-                entropy_max,
-                self.train_step
-            )
-            self.logger.log_scalar(
-                "Entropy/Lowest",
-                entropy_min,
-                self.train_step
-            )
-
-            self.logger.log_scalar(
-                "Alpha/Mean Value",
-                sum(alphs) / actor_steps,
-                self.train_step
-            )
-            self.logger.log_scalar(
-                "Alpha/Mean Loss",
-                sum(alph_losses) / actor_steps,
-                self.train_step
-            )
-
-            self.logger.log_scalar(
-                "Position/X Means",
-                sum(pos_means_x) / actor_steps,
-                self.train_step
-            )
-            self.logger.log_scalar(
-                "Position/X Stds",
-                sum(pos_stds_x) / actor_steps,
-                self.train_step
-            )
-            self.logger.log_scalar(
-                "Position/Y Means",
-                sum(pos_means_y) / actor_steps,
-                self.train_step
-            )
-            self.logger.log_scalar(
-                "Position/Y Stds",
-                sum(pos_stds_y) / actor_steps,
-                self.train_step
-            )
-
-        print(f"Critic/Mean Loss: {sum(c_losses) / critic_steps}")
-        print(f"Critic/Highest Loss: {c_loss_max}")
-        print(f"Critic/Lowest Loss: {c_loss_min}")
-
-        print(f"Q/Mean Value: {sum(q_vals) / critic_steps}")
-        print(f"Q/Highest Value: {q_max}")
-        print(f"Q/Lowest Value: {q_min}")
-
-        if self.iters >= consts.CRITIC_ONLY:
-            print(f"Actor/Mean Loss: {sum(a_losses) / actor_steps}")
-            print(f"Actor/Highest Loss: {a_loss_max}")
-            print(f"Actor/Lowest Loss: {a_loss_min}")
-
-            print(f"Entropy/Mean: {sum(entropies) / actor_steps}")
-            print(f"Entropy/Highest: {entropy_max}")
-            print(f"Entropy/Lowest: {entropy_min}")
-
-            print(f"Alpha/Mean Value: {sum(alphs) / actor_steps}")
-            print(f"Alpha/Mean Loss: {sum(alph_losses) / actor_steps}")
-
-            print(f"Position/X Means: {sum(pos_means_x) / actor_steps}")
-            print(f"Position/X Stds: {sum(pos_stds_x) / actor_steps}")
-            print(f"Position/Y Means: {sum(pos_means_y) / actor_steps}")
-            print(f"Position/Y Stds: {sum(pos_stds_y) / actor_steps}")
-
-        print()
-
-        self.train_step += 1
-
-    def run_training(
-            self, num_iters, num_train_steps, batch_min_steps, max_ep_len
-    ):
-        self.tot_steps = 0
-
-        for i in range(num_iters):
-            print(f"Iteration {i}")
-            print(f"Total Steps: {self.tot_steps}")
-            print("Collecting Rollouts...")
-
-            # simulate rollouts until minimum batch number of steps
-            rolls, iter_steps = self.sim_rolls(batch_min_steps, max_ep_len)
-            self.tot_steps += iter_steps
-
-            self.rebuff.add_rolls(rolls)
-
-            print("Running updates...")
-            self.train(num_train_steps)
-
-            if i % consts.ITERS_PER_EVAL == 0:
+            if self.tot_steps % consts.ITERS_PER_EVAL == 0:
                 print("Evaluating...")
-                self.sim_n_rolls(5, max_ep_len, render=True)
+                self.sim_roll(max_ep_len, render=True)
 
-            if i % consts.SAVE_EVERY == 0:
+            if self.tot_steps % consts.SAVE_EVERY == 0:
                 print("Saving...")
-                self.logger.save_models(self.p_pol, self.e_pol, i)
+                self.logger.save_models(self.p_pol, self.e_pol, self.tot_steps)
                 print()
 
-            if i % consts.ENEMY_UPDATE == 0 and i != 0:
+            if self.tot_steps % consts.ENEMY_UPDATE == 0 and self.tot_steps != 0:
                 print("Updating Enemy...")
                 self.e_pol.update_policy(self.p_pol)
                 print()
 
-            self.iters += 1
+            # increment steps
+            steps += 1
+            self.tot_steps += 1
 
 
 trainer = Trainer()
-# trainer.sim_roll(1080, 0.0, True)
-trainer.run_training(1000, consts.TRAIN_STEPS, 10000, 1080)
-# game = Game()
-# game.reset()
-# game.run()
+trainer.train()
