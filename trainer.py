752faada993ae6e5a56bf7fc394800b9ef67bd22
Joshua Boisvert <joshmboisvert@gmail.com>
2025-06-04 13:51:41 -0700

    no enemy movement

--- trainer.py
+++ trainer.py
@@ -28,19 +28,55 @@ class Trainer:
         self.ac_mask = [0, 0, 1, 0, 0]
         self.game = Game(True, ac_mask=self.ac_mask)
 
-    def sim_roll(self, max_ep_len=1080, render=False):
-        # initialize steps and frames
-        steps = 1
+    def train(self, max_ep_len=1080):
+        # done flag for completion of episode
+        done = True
+
+        # frames for rendering
         frames = []
 
-        # get initial game state
-        ob = self.game.reset()
+        # first action flag
+        first_ac = True
 
-        # initialize capturing lists
-        p_obs = []
-        e_obs = []
+        # accumulated reward
+        accum_p_rew = None
 
-        while True:
+        # rendering flag
+        render = True
+        next_render = True
+        while self.tot_steps < self.max_steps:
+            if done:
+                # initialize steps and frames
+                steps = 1
+
+                # get initial game state
+                ob = self.game.reset()
+
+                # initialize reward accumulation
+                tot_p_rew, tot_e_rew = 0.0, 0.0
+
+                # add episode return
+                if accum_p_rew is not None:
+                    self.logger.log_scalar(
+                        "Eval Return",
+                        accum_p_rew,
+                        self.tot_steps
+                    )
+                    print(
+                        f"Eval Return: {accum_p_rew}"
+                    )
+
+                # reset total rewards
+                accum_p_rew = 0.0
+
+                # reset done and first
+                done = False
+                first_ac = True
+
+                # initialize capturing lists
+                p_obs = deque(maxlen=consts.WINDOW)
+                e_obs = deque(maxlen=consts.WINDOW)
+
             # add observations
             p_obs.append(ob["player"])
             e_obs.append(ob["enemy"])
@@ -54,78 +90,7 @@ class Trainer:
             if steps % consts.FPA == 0 and steps >= consts.WINDOW:
                 # poll policy
                 p_ac = self.p_pol.get_action(p_obs[-1], self.ac_mask)
-                e_ac = self.e_pol.get_action(e_obs[-1], self.ac_mask)
-
-            else:
-                self.p_pol.update_actor_hidden(p_obs[-1])
-                self.e_pol.update_actor_hidden(e_obs[-1])
-
-            # move forward step
-            ob, rew, done, info = self.game.step(
-                playing=False, p_ac=p_ac, e_ac=e_ac
-            )
-
-            # end episode if someone dies or hit max episode length
-            if steps >= max_ep_len or done:
-                break
-
-            # increment steps
-            steps += 1
-
-        p_obs.append(ob["player"])
-        e_obs.append(ob["enemy"])
-
-        if render:
-            self.logger.log_video(frames, f"{self.tot_steps}.mp4")
-
-    def train(self, max_ep_len=1080):
-        done = True
-        first = True
-        accum_p_rew_list = deque(maxlen=10)
-
-        while self.tot_steps < self.max_steps:
-            if done:
-                # initialize steps and frames
-                steps = 1
-
-                # get initial game state
-                ob = self.game.reset()
-
-                # initialize reward accumulation
-                tot_p_rew, tot_e_rew = 0.0, 0.0
-
-                # reset total rewards
-                accum_p_rew = 0.0
-                if len(accum_p_rew_list) > 0:
-
-                    self.logger.log_scalar(
-                        "Eval/Mean Return",
-                        sum(accum_p_rew_list) / len(accum_p_rew_list),
-                        self.tot_steps
-                    )
-                    print(
-                        f"Eval/Mean Return: {sum(accum_p_rew_list) / len(accum_p_rew_list)}"
-                    )
-
-                # reset done and first
-                done = False
-                first = True
-
-                # initialize capturing lists
-                p_obs = deque(maxlen=consts.WINDOW)
-                e_obs = deque(maxlen=consts.WINDOW)
-
-            # add observations
-            p_obs.append(ob["player"])
-            e_obs.append(ob["enemy"])
-
-            p_ac, e_ac = None, None
-
-            # get action and take it if acting frame
-            if steps % consts.FPA == 0 and steps >= consts.WINDOW:
-                # poll policy
-                p_ac = self.p_pol.get_action(p_obs[-1], self.ac_mask)
-                e_ac = self.e_pol.get_action(e_obs[-1], self.ac_mask)
+                e_ac = self.e_pol.get_action(e_obs[-1], [1, 0, 0, 0, 0])
 
             else:
                 self.p_pol.update_actor_hidden(p_obs[-1])
@@ -139,11 +104,11 @@ class Trainer:
             # only when action taking turn
             if p_ac is not None and e_ac is not None:
                 # add to replay buffer
-                if first:
+                if first_ac:
                     self.rebuff.add(
                         list(p_obs), p_ac[0], p_ac[1], tot_p_rew, done
                     )
-                    first = False
+                    first_ac = False
                 else:
                     self.rebuff.add(
                         list(p_obs)[-consts.FPA:], p_ac[0],
@@ -176,11 +141,16 @@ class Trainer:
                 # add rewards for last action
                 self.rebuff.reassign_rew(tot_p_rew)
 
-                # add accumulated rewards
-                accum_p_rew_list.append(accum_p_rew)
-
                 # set done
                 done = True
+
+                # set render
+                if render:
+                    render = False
+
+                if next_render:
+                    render = True
+                    next_render = False
 
             if (
                 steps % consts.FPA == 0 and
@@ -190,12 +160,12 @@ class Trainer:
                 c_metrics = self.p_pol.update_critic(batch, self.ac_mask)
 
                 self.logger.log_scalar(
-                    "Critic Loss",
+                    "Critic/Loss",
                     c_metrics["critic_loss"],
                     self.tot_steps
                 )
                 self.logger.log_scalar(
-                    "Mean Q Value",
+                    "Critic/Q Value",
                     c_metrics["q_values"],
                     self.tot_steps
                 )
@@ -209,34 +179,34 @@ class Trainer:
                     a_metrics = self.p_pol.update_actor(batch, self.ac_mask)
 
                     self.logger.log_scalar(
-                        "Actor Loss",
+                        "Actor/Loss",
                         a_metrics["actor_loss"],
                         self.tot_steps
                     )
 
                     self.logger.log_scalar(
-                        "Entropy",
+                        "Actor/Entropy",
                         a_metrics["entropy"],
                         self.tot_steps
                     )
 
                     self.logger.log_scalar(
-                        "Type Alpha Value",
+                        "Actor/Type Alpha Value",
                         a_metrics["type_alpha"],
                         self.tot_steps
                     )
                     self.logger.log_scalar(
-                        "Type Alpha Value",
+                        "Actor/Type Alpha Value",
                         a_metrics["pos_alpha"],
                         self.tot_steps
                     )
                     self.logger.log_scalar(
-                        "Type Alpha Loss",
+                        "Actor/Type Alpha Loss",
                         a_metrics["type_alpha_loss"],
                         self.tot_steps
                     )
                     self.logger.log_scalar(
-                        "Pos Alpha Loss",
+                        "Actor/Pos Alpha Loss",
                         a_metrics["pos_alpha_loss"],
                         self.tot_steps
                     )
@@ -264,14 +234,17 @@ class Trainer:
 
             if self.tot_steps % consts.ITERS_PER_EVAL == 0:
                 print("Evaluating...")
-                self.sim_roll(max_ep_len, render=True)
+                next_render = True
 
             if self.tot_steps % consts.SAVE_EVERY == 0:
                 print("Saving...")
                 self.logger.save_models(self.p_pol, self.e_pol, self.tot_steps)
                 print()
 
-            if self.tot_steps % consts.ENEMY_UPDATE == 0 and self.tot_steps != 0:
+            if (
+                self.tot_steps % consts.ENEMY_UPDATE == 0 and
+                self.tot_steps != 0
+            ):
                 print("Updating Enemy...")
                 self.e_pol.update_policy(self.p_pol)
                 print()
