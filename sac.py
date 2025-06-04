6d1012b945586ec0ba52729bd22ca9f64d8f8255
Joshua Boisvert <joshmboisvert@gmail.com>
2025-06-03 20:31:40 -0700

    remove erroneous print

--- sac.py
+++ sac.py
@@ -32,7 +32,6 @@ class CNNLSTM(nn.Module):
             dummy = torch.zeros(1, 3, 200, 150)
             conv_out = self.conv(dummy)
             flat_dim = conv_out.shape[1]
-            print(flat_dim)
 
         self.fc = nn.Linear(flat_dim, feature_dim)
 
