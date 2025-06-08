dc64c8d3dcae85c19307736a02f3ef0cb0a45343
Joshua Boisvert <joshmboisvert@gmail.com>
2025-06-08 17:45:25 -0400

    changed target entropy

--- pt_pol.py
+++ pt_pol.py
@@ -54,7 +54,7 @@ class PTPolicy:
             self.type_alph_opt = optim.Adam([self.log_type_alph], lr=lr)
             self.pos_alph_opt = optim.Adam([self.log_pos_alph], lr=lr)
             self.type_target_entropy = 1.069
-            self.pos_target_entropy = 0
+            self.pos_target_entropy = 2.0
 
     def make_device_tensor(self, arr, dtype=None):
         return torch.as_tensor(arr, dtype=dtype, device=self.device)
